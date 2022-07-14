d5c70f35fd549ae9d39d87e40d5657f9e3abfcd6
Bitaw <stickynotes5@yahoo.com>
2022-07-13 19:48:05 -0500

    Added variables to script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,36 @@
 // Assignment code here
 
-let Question = prompt("Would you like your password to have a length criteria? Type Yes or No.");
-if (Question != null) {
-  if (Question.toLowerCase == "yes") {
-    var lengthcrit = true;
+
+
+//Prompt method
+//more prompts Confirm method
+var CharLength = prompt("How many characters do you want in your password?\nPick a number between 8 and 128");
+
+var lowercase = confirm("Do you want your password to have lowercase characters?")
+
+var uppercase = confirm("Do you want your password to have uppercase characters?")
+
+var numbers = confirm("Do you want your password to have numbers?")
+
+var SpecialChar = confirm("Do you want you password to have special characters?")
+
+
+
+function generatePassword () {
+  console.log("Test");
+
+  // 1)Prompt for password criteria
+  // a) length 8-128 characters, Prompt method
+  // b) lowercase, uppercase, numbers, special characters, Confirm method
+  // 2) Validate input
+  // 3) Generate password based on criteria
+  // 4) Display Generated password
+  
+
+  return "hi"
+}
+
+
   
 
 
