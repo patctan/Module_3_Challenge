c7d2e969cd0cdd15805287cb5610ed17c2cd115e
Bitaw <stickynotes5@yahoo.com>
2022-07-17 15:39:02 -0500

    Added arrays and variable for user's choices to script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,44 +1,66 @@
 // Assignment code here
 
-
-function generatePassword() {
-  console.log("Test");
-
-  var HowLong = prompt("How many characters do you want in your password?\nPick a number between 8 and 128")
-
-  var PasswordLength = parseInt(HowLong)
-
-  if (isNaN(PasswordLength) || PasswordLength <=8 || PasswordLength >=128) {
-    alert("Invalid input. Please enter a number between 8 and 128.")
-    return
-  }
-}
-
-//var lowercase = confirm("Do you want your password to have lowercase characters?");
-
-//var uppercase = confirm("Do you want your password to have uppercase characters?");
-
-//var numbers = confirm("Do you want your password to have numbers?");
-
-//var SpecialChar = confirm("Do you want you password to have special characters?");
-
-
-
-
-
   // 1)Prompt for password criteria
   // a) length 8-128 characters, Prompt method
   // b) lowercase, uppercase, numbers, special characters, Confirm method
   // 2) Validate input
   // 3) Generate password based on criteria
   // 4) Display Generated password
-  
 
-  
+function generatePassword() {
+  console.log("Test");
+
+  var HowLong = prompt("How many characters do you want in your password?\nPick a number between 8 and 128");
+
+  var PasswordLength = parseInt(HowLong);
+
+  if (isNaN(PasswordLength) || PasswordLength < 8 || PasswordLength > 128) {
+    alert("Invalid input. Please enter a number between 8 and 128.");
+    return;
+  }
+
+  var Lowercase = confirm("Do you want your password to have lowercase characters?");
+
+  var Uppercase = confirm("Do you want your password to have uppercase characters?");
+
+  var Numbers = confirm("Do you want your password to have numbers?");
+
+  var SpecialChar = confirm("Do you want you password to have special characters?");
 
 
+  var LowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-  
+  var NumberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+  var SpecialCharList = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "~", "`"]
+
+  var UppercaseList = []
+
+  for (var i=0; i < LowercaseList.length; i++ ) {
+    UppercaseList = LowercaseList[i].toUpperCase;
+  }
+
+  var UsersChoices = []
+
+  if (Lowercase == true) {
+    UsersChoices.push(LowercaseList);
+  }
+
+  if (Uppercase == true) {
+    UsersChoices.push(UppercaseList);
+  }
+
+  if (Numbers == true) {
+    UsersChoices.push(NumberList);
+  }
+
+  if (SpecialChar == true) {
+    UsersChoices.push(SpecialCharList);
+  }
+}
+ 
+
+    
 
 
 // Get references to the #generate element
